eebb45f13a1bf0267cdfd034aca3bfdc5daceb28
Jan-Hendrik Peters <hennr@hennr.name>
2017-09-20 23:35:56 +0200

    bump to jessie vagrant image

--- Vagrantfile
+++ Vagrantfile
@@ -2,7 +2,7 @@
 # vi: set ft=ruby :
 
 Vagrant.configure(2) do |config|
-  config.vm.box = "debian/contrib-jessie64"
+  config.vm.box = "debian/contrib-stretch64"
   config.vm.network "private_network", ip: "192.168.23.42"
   config.vm.provider "virtualbox" do |vb|
       vb.memory = "256"
--- provision-vm.sh
+++ provision-vm.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# add jessie backports for openJDK 8
-sudo echo "
-deb http://httpredir.debian.org/debian jessie main
-deb http://security.debian.org/ jessie/updates main
-deb http://ftp.debian.org/debian jessie-backports main
-" > /etc/apt/sources.list
-
-# activate backports repo
-sudo apt-get update
-
 # install java 8
+sudo apt-get update
 sudo apt-get install openjdk-8-jre-headless --yes --no-install-recommends
 
 # install the spring boot app
-sudo dpkg -i /vagrant/target/*.deb
+if [ -f /vagrant/target/*.deb ]
+then
+  sudo dpkg -i /vagrant/target/*.deb;
+else
+  echo "ERROR: No *.deb file found. Did you run 'mvn package already?'"
+  exit 1
+fi
 
 echo "app is up and running under http://192.168.23.42:8080/health"
